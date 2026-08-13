1aad0e4494d63aaf2fcb39e6e61e5b56ce235e6c
TimBLukas <tim.b.lukas@gmail.com>
2026-08-13 15:40:06 +0200

    improved glass look

--- purrism/neovim.lua
+++ purrism/neovim.lua
@@ -5,10 +5,18 @@ return {
 		priority = 1000,
 
 		config = function()
+			-- Disable LSP semantic tokens so language servers don't re-color functions/parameters
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client then
+						client.server_capabilities.semanticTokensProvider = nil
+					end
+				end,
+			})
+
 			require("catppuccin").setup({
 				flavour = "mocha",
-
-				-- Let Kitty + Hyprland provide the glass.
 				transparent_background = true,
 
 				float = {
@@ -16,127 +24,132 @@ return {
 					solid = false,
 				},
 
-				-- Mostly monochrome palette
+				-- Ultra-minimal monochrome palette
 				color_overrides = {
 					mocha = {
-						rosewater = "#FFFFFF",
-						flamingo = "#E8E8E8",
-						pink = "#DCDCDC",
-						mauve = "#C8C8C8",
+						-- Foreground
+						text = "#E6E8EB",
+						subtext1 = "#B8BDC5",
+						subtext0 = "#949AA3",
 
-						-- Very subtle semantic colors
-						red = "#D8A0A0",
-						maroon = "#C99090",
-						peach = "#D8B49A",
-						yellow = "#D8D0A0",
-						green = "#A8C0A8",
-						teal = "#A0BABA",
-						sky = "#A8BCC8",
-						sapphire = "#9EAFBA",
-						blue = "#AAB8C8",
-						lavender = "#B8B8C8",
+						-- Overlays
+						overlay2 = "#737982",
+						overlay1 = "#5F656D",
+						overlay0 = "#4A4F57",
 
-						-- Text hierarchy
-						text = "#FFFFFF",
-						subtext1 = "#D6D6D6",
-						subtext0 = "#A8A8A8",
-
-						-- Gray UI
-						overlay2 = "#888888",
-						overlay1 = "#686868",
-						overlay0 = "#4A4A4A",
-
-						-- Glass surfaces
-						surface2 = "#2A2A2A",
-						surface1 = "#1C1C1C",
-						surface0 = "#141414",
+						-- Surfaces
+						surface2 = "#363A40",
+						surface1 = "#292D32",
+						surface0 = "#22262B",
 
 						-- Background
-						base = "#0B0B0D",
-						mantle = "#08080A",
-						crust = "#050507",
+						base = "#17191D",
+						mantle = "#131519",
+						crust = "#0F1114",
+
+						-- Semantic colors → monochrome
+						rosewater = "#E6E8EB",
+						flamingo = "#E6E8EB",
+						pink = "#D4D7DC",
+						mauve = "#D4D7DC",
+
+						red = "#B8BDC5",
+						maroon = "#B8BDC5",
+
+						peach = "#B8BDC5",
+						yellow = "#B8BDC5",
+						green = "#B8BDC5",
+						teal = "#B8BDC5",
+
+						sky = "#D4D7DC",
+						sapphire = "#D4D7DC",
+						blue = "#D4D7DC",
+						lavender = "#E6E8EB",
 					},
 				},
 
-				-- Minimal syntax highlighting
 				custom_highlights = function(colors)
+					local text = { fg = colors.text }
+					local muted = { fg = colors.subtext1 }
+					local comment = { fg = colors.overlay2, italic = true }
+
 					return {
-						-- Main code
-						["@variable"] = { fg = colors.text },
-						["@variable.builtin"] = { fg = colors.text },
-						["@variable.parameter"] = { fg = colors.text },
-						["@variable.member"] = { fg = colors.text },
+						["@variable"] = text,
+						["@variable.builtin"] = text,
+						["@variable.parameter"] = text,
+						["@variable.member"] = text,
+						["@function"] = text,
+						["@function.call"] = text,
+						["@function.method"] = text,
+						["@function.method.call"] = text,
+						["@function.builtin"] = text,
+						["@function.macro"] = text,
+						["@parameter"] = text,
+						["@parameter.reference"] = text,
+						["@property"] = text,
+						["@field"] = text,
+						["@type"] = text,
+						["@type.builtin"] = text,
+						["@type.definition"] = text,
+						["@type.qualifier"] = text,
+						["@constructor"] = text,
+						["@keyword"] = text,
+						["@keyword.function"] = text,
+						["@keyword.return"] = text,
+						["@keyword.operator"] = text,
+						["@keyword.import"] = text,
+						["@keyword.conditional"] = text,
+						["@keyword.repeat"] = text,
+						["@constant"] = text,
+						["@constant.builtin"] = text,
+						["@constant.macro"] = text,
+						["@number"] = text,
+						["@boolean"] = text,
+						["@label"] = text,
+						["@tag"] = text,
+						["@tag.attribute"] = text,
+						["@markup.heading"] = { fg = colors.text, bold = true },
+						["@markup.bold"] = { fg = colors.text, bold = true },
+						["@markup.italic"] = { fg = colors.subtext1, italic = true },
 
-						-- Functions
-						["@function"] = { fg = colors.text },
-						["@function.call"] = { fg = colors.text },
-						["@function.method"] = { fg = colors.text },
-						["@function.method.call"] = { fg = colors.text },
+						-- Punctuation, operators, strings
+						["@operator"] = muted,
+						["@punctuation.bracket"] = muted,
+						["@punctuation.delimiter"] = muted,
+						["@punctuation.special"] = muted,
+						["@tag.delimiter"] = muted,
+						["@string"] = muted,
+						["@string.escape"] = muted,
+						["@string.special"] = muted,
 
-						-- Types / classes
-						["@type"] = { fg = colors.text },
-						["@type.builtin"] = { fg = colors.text },
-						["@type.definition"] = { fg = colors.text },
-						["@constructor"] = { fg = colors.text },
+						["@comment"] = comment,
 
-						-- Parameters
-						["@parameter"] = { fg = colors.text },
-						["@parameter.reference"] = { fg = colors.text },
+						Function = text,
+						Identifier = text,
+						Statement = text,
+						Type = text,
+						Constant = text,
+						PreProc = text,
+						String = muted,
+						Comment = comment,
+						Special = muted,
+						Delimiter = muted,
+						Operator = muted,
 
-						-- Properties / fields
-						["@property"] = { fg = colors.text },
-						["@field"] = { fg = colors.text },
-
-						-- Keywords
-						["@keyword"] = { fg = colors.text },
-						["@keyword.function"] = { fg = colors.text },
-						["@keyword.return"] = { fg = colors.text },
-						["@keyword.operator"] = { fg = colors.text },
-
-						-- Operators / punctuation
-						["@operator"] = { fg = colors.subtext1 },
-						["@punctuation.bracket"] = { fg = colors.subtext1 },
-						["@punctuation.delimiter"] = { fg = colors.subtext1 },
-						["@punctuation.special"] = { fg = colors.subtext1 },
-
-						-- Constants / numbers / booleans
-						["@constant"] = { fg = colors.text },
-						["@constant.builtin"] = { fg = colors.text },
-						["@number"] = { fg = colors.text },
-						["@boolean"] = { fg = colors.text },
-
-						-- Strings
-						["@string"] = { fg = colors.subtext1 },
-						["@string.escape"] = { fg = colors.subtext1 },
-						["@string.special"] = { fg = colors.subtext1 },
-
-						-- Comments
-						["@comment"] = {
-							fg = colors.overlay2,
-							italic = true,
-						},
-
-						-- Labels / tags
-						["@label"] = { fg = colors.text },
-						["@tag"] = { fg = colors.text },
-						["@tag.attribute"] = { fg = colors.text },
-						["@tag.delimiter"] = { fg = colors.subtext1 },
-
-						-- Markdown / headings
-						["@markup.heading"] = {
-							fg = colors.text,
-							bold = true,
-						},
-
-						["@markup.italic"] = {
-							fg = colors.subtext1,
-							italic = true,
-						},
-
-						["@markup.bold"] = {
-							fg = colors.text,
-							bold = true,
-						},
+						["@lsp.type.function"] = text,
+						["@lsp.type.method"] = text,
+						["@lsp.type.parameter"] = text,
+						["@lsp.type.variable"] = text,
+						["@lsp.type.property"] = text,
+						["@lsp.type.keyword"] = text,
+						["@lsp.type.type"] = text,
+						["@lsp.type.class"] = text,
+						["@lsp.type.namespace"] = text,
+						["@lsp.type.enum"] = text,
+						["@lsp.type.enumMember"] = text,
+						["@lsp.type.interface"] = text,
+						["@lsp.type.struct"] = text,
+						["@lsp.type.macro"] = text,
 					}
 				end,
 
@@ -147,23 +160,19 @@ return {
 					treesitter = true,
 					telescope = true,
 					which_key = true,
-
 					indent_blankline = {
 						enabled = true,
 						scope_color = "overlay2",
 						colored_indent_levels = false,
 					},
-
 					native_lsp = {
 						enabled = true,
-
 						virtual_text = {
 							errors = { "italic" },
 							hints = { "italic" },
 							warnings = { "italic" },
 							information = { "italic" },
 						},
-
 						underlines = {
 							errors = { "undercurl" },
 							hints = { "undercurl" },
